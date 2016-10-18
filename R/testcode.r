620e49b50a8273773fa607be11011cfb4db61759
Adrian Hordyk <ar.hordyk@gmail.com>
2016-10-18 13:20:12 +0800

    comment out testcode

--- R/testcode.r
+++ R/testcode.r
@@ -1,12 +1,12 @@
-library(LBSPR)
-LB_pars <- new("LB_pars")
-LB_pars@MK <- 1.5
-LB_pars@Linf <- 100
-LB_pars@L50 <- 50
-LB_pars@L95 <- 55 
-LB_pars@FM <- 4
-LB_pars@SL50 <- 0
-LB_pars@SL95 <- 1
+# library(LBSPR)
+# LB_pars <- new("LB_pars")
+# LB_pars@MK <- 1.5
+# LB_pars@Linf <- 100
+# LB_pars@L50 <- 50
+# LB_pars@L95 <- 55 
+# LB_pars@FM <- 4
+# LB_pars@SL50 <- 0
+# LB_pars@SL95 <- 1
 
-tt <- LBSPRsim(LB_pars)
+# tt <- LBSPRsim(LB_pars)
 
